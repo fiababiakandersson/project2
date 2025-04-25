2352276318b7a90e2595dccfe9ec927906eddb02
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-04-25 18:00:11 +0200

    dynamically render colours

--- logic.js
+++ logic.js
@@ -1,3 +1,29 @@
+window.addEventListener("load", function () {
+  const tag = document.querySelectorAll(".filter-tag");
+
+  tag.forEach((item) => {
+    const text = item.textContent.trim().toLowerCase();
+
+    switch (text) {
+      case "fisk":
+        item.classList.add("red");
+        break;
+      case "huvudrätt":
+      case "asiatisk":
+        item.classList.add("purple");
+        break;
+      case "glutenfritt":
+        item.classList.add("yellow");
+        break;
+      case "kan göras vegetarisk":
+        item.classList.add("green");
+        break;
+      default:
+        break;
+    }
+  });
+});
+
 document.querySelectorAll(".menuItem").forEach((item) => {
   item.addEventListener("click", function (e) {
     const text = e.target.innerText.trim();
